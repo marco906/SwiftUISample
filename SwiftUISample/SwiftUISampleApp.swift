55b311f6b33b5e49f90a5e750b53b6ddaac2465d
Marco <marco@mwaretechnologies.com>
2024-03-08 17:56:46 +0100

    Initial Commit

---
+++ SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by Marco Wenzel on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
