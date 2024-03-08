f2e7723aeae83b3bc18a63516a9675517b06c8f2
Marco <marco@mwaretechnologies.com>
2024-03-08 19:56:17 +0100

    add user model

---
+++ SwiftUISample/Data/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SwiftUISample
+//
+//  Created by Marco Wenzel on 08/03/2024.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var email: String
+    var birthday: Date
+}
