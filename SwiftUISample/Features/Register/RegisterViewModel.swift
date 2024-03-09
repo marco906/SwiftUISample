6f218dd06eb8a70980973da2ba3d904d39848674
marco906 <mar.wenzel@gmx.de>
2024-03-09 15:16:22 +0100

    add input field validation for register model

--- SwiftUISample/Features/Register/RegisterViewModel.swift
+++ SwiftUISample/Features/Register/RegisterViewModel.swift
@@ -12,8 +12,58 @@ class RegisterViewModel: ObservableObject {
     @Published var email = ""
     @Published var birthday = Date.now
     
+    func validateName() throws {
+        guard !name.isEmpty else {
+            throw ValidationError.invalidName
+        }
+    }
+    
+    func validateEmail() throws {
+        let pattern = /([a-zA-Z0-9._-]+)(@{1})([a-zA-Z0-9.-]+)(.{1})([a-zA-Z]{2,})/
+        guard email.wholeMatch(of: pattern) != nil else {
+            throw ValidationError.invalidEmail
+        }
+    }
+    
+    func validateBirthday() throws {
+        let calendar = Calendar.current
+        let minDateComponents = DateComponents(year: 1900, month: 1, day: 1)
+        let maxDateComponents = DateComponents(year: 2022, month: 12, day: 31)
+        
+        guard let minDate = calendar.date(from: minDateComponents), let maxDate = calendar.date(from: maxDateComponents) else {
+            throw ValidationError.invalidBirthday
+        }
+        
+        guard birthday >= minDate && birthday <= maxDate else {
+            throw ValidationError.invalidBirthday
+        }
+    }
+    
+    private func validate() throws {
+        try validateName()
+        try validateEmail()
+        try validateBirthday()
+    }
+    
+    private func save() {
+        // TODO: save user object
+    }
+    
     func register() {
-        // TODO: validate
+        do {
+            try validate()
+        } catch {
+            // TODO: handle errors
+        }
+        save()
         // TODO: push to welcome screen
     }
+}
+
+enum ValidationError: LocalizedError {
+    case invalidName
+    case invalidEmail
+    case invalidBirthday
+    
+    // TODO: Add error descriptions
 }
